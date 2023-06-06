dc4dbc6ebd3187b4562cf85b3e0322255f173aca
kannbo <113285773+kannbo@users.noreply.github.com>
2023-06-06 21:08:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+a=[]
+b=""
 st.title("hello")
 text=st.text_input("入力","hello")
 st.markdown(text)
+a.append(text)
+b=""
+for i in a:
+  b=b+i+"\n"
+st.markdown(b)
